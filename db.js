82cd668a74b320bc2116b5d25df0238e9cc8f2a7
danypro5555 <158477900+danypro5555@users.noreply.github.com>
2024-11-16 11:10:17 +0800

    Update db.js

--- db.js
+++ db.js
@@ -1,6 +1,7 @@
 const mysql = require('mysql')
 const conn = mysql.createConnection({
     host:'127.0.0.1',
+    port: 3306,
     user: 'root',
     password: 'Css10388708002x',
     database: 'summoner_info'
