1cbdb9d7dfb01e3f14c17dcabad511fc54a5ff45
danypro5555 <danypro5555@gmail.com>
2024-11-16 09:35:10 +0800

    fixed

--- db.js
+++ db.js
@@ -1,8 +1,8 @@
 const mysql = require('mysql')
 const conn = mysql.createConnection({
-    host:'localhost',
+    host:'127.0.0.1',
     user: 'root',
-    password: '',
+    password: 'Css10388708002x',
     database: 'summoner_info'
 }) 
 
